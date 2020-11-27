4dfb9bf4bdf7d944a4d32d75af52e60ed7b39bf6
Alexey Ushakov <kafka.pochta@gmail.com>
2020-11-27 21:53:51 +0300

    WIP: update upfirdn2d_native

--- models/stylegan2/op/upfirdn2d.py
+++ models/stylegan2/op/upfirdn2d.py
@@ -157,8 +157,11 @@ def upfirdn2d(input, kernel, up=1, down=1, pad=(0, 0)):
 
 
 def upfirdn2d_native(
-        input, kernel, up_x, up_y, down_x, down_y, pad_x0, pad_x1, pad_y0, pad_y1
+    input, kernel, up_x, up_y, down_x, down_y, pad_x0, pad_x1, pad_y0, pad_y1
 ):
+    _, channel, in_h, in_w = input.shape
+    input = input.reshape(-1, in_h, in_w, 1)
+
     _, in_h, in_w, minor = input.shape
     kernel_h, kernel_w = kernel.shape
 
@@ -170,11 +173,11 @@ def upfirdn2d_native(
         out, [0, 0, max(pad_x0, 0), max(pad_x1, 0), max(pad_y0, 0), max(pad_y1, 0)]
     )
     out = out[
-          :,
-          max(-pad_y0, 0): out.shape[1] - max(-pad_y1, 0),
-          max(-pad_x0, 0): out.shape[2] - max(-pad_x1, 0),
-          :,
-          ]
+        :,
+        max(-pad_y0, 0) : out.shape[1] - max(-pad_y1, 0),
+        max(-pad_x0, 0) : out.shape[2] - max(-pad_x1, 0),
+        :,
+    ]
 
     out = out.permute(0, 3, 1, 2)
     out = out.reshape(
@@ -189,5 +192,9 @@ def upfirdn2d_native(
         in_w * up_x + pad_x0 + pad_x1 - kernel_w + 1,
     )
     out = out.permute(0, 2, 3, 1)
+    out = out[:, ::down_y, ::down_x, :]
 
-    return out[:, ::down_y, ::down_x, :]
+    out_h = (in_h * up_y + pad_y0 + pad_y1 - kernel_h) // down_y + 1
+    out_w = (in_w * up_x + pad_x0 + pad_x1 - kernel_w) // down_x + 1
+
+    return out.view(-1, channel, out_h, out_w)
